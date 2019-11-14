2812cc2fdb3900c914f75951268f2543af44ce79
Pietro Passarelli <pietro.passarelli@wsj.com>
2019-11-14 12:34:38 +0000

    Refactored

--- src/example-usage.js
+++ src/example-usage.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const awsToDpe = require('./index.js');
 
-const awsTranscript = require('./sample/WSJ8623212922.mp3.json');
+const awsTranscript = require('../sample/WSJ8623212922.mp3.json');
 
 const res = awsToDpe(awsTranscript);
 
